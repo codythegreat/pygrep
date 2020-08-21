cbdc4967748821499a3607221445bf76b05a7ff7
Cody Maxie <38113810+codythegreat@users.noreply.github.com>
2020-08-20 20:38:35 -0500

    added file extension filtering and program loop

--- pygrep.py
+++ pygrep.py
@@ -2,14 +2,21 @@
 #             the search parameter. Prints result as filename:linenumber:linecontent
 import os
 
-searchTerm = input("Search Term: ")
-print(f"Searching for {searchTerm}...")
+while __name__ == "__main__":
+    searchString = input("Search String: ")
+    fileType     = input("Extension: ")
+    print(f"Searching for {searchString} of filetype {fileType}...")
 
-for root, dirs, files in os.walk(".", topdown=False):
-    for name in files:
-        with open(os.path.join(root, name), 'r') as f:
-            lineNumber = 1
-            while line := f.readline():
-                if searchTerm in line:
-                    print(f'{os.path.join(root, name)}:{lineNumber}:{line}', end='')
-                lineNumber += 1
+    for root, dirs, files in os.walk(".", topdown=False):
+        for name in files:
+            if fileType == '*' or name.endswith(fileType):
+                try:
+                    with open(os.path.join(root, name), 'r') as f:
+                        lineNumber = 1
+                        while line := f.readline():
+                            if searchString in line:
+                                print(f'{os.path.join(root, name)}:{lineNumber}:{line}', end='')
+                            lineNumber += 1
+                except:
+                    print(f'Error at file: {os.path.join(root, name)}')
+                    continue
